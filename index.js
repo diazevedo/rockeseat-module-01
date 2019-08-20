ade0539598f18242d2318d388de78319a3c53456
Diego Azevedo <dapptn@hotmail.com>
2019-08-20 22:55:40 +1000

    Created Post, Put, and Delete routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 //express exports a function
 
 const server = express();
+//Tells to express use json
+server.use(express.json());
 /**
  * Query params =>  ?name=diego => req.query
  * Route params => /users/125 => req.params
- * Request body => Data coming from Post or PUT (usually)
+ * Request body => Data coming from Post or PUT (usually) =>
  *
  * res.send() => To send a plain text return
  * res.json({}) => To send an object
@@ -27,5 +29,25 @@ server.get("/users/:index", (req, res) => {
   res.json(users[index]);
 });
 
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  //returns a 200 code status
+  res.send();
+});
 //localhost:3000/test
 server.listen(3000);
